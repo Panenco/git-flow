c79de639436423bb0aa8aa0b4fbe8f119044c17e
Kostiantyn Doichev <kostiantyn.doichev@panenco.com>
2023-04-27 11:33:20 +0200

    feat: feature name

--- src/feature-name.ts
+++ src/feature-name.ts
@@ -1,6 +1,46 @@
 import core from "@actions/core"
 import github from "@actions/github"
 
-const context = github.context
+const {ref, eventName, payload} = github.context
 
-core.setOutput("feature_name", JSON.stringify(context))
+const delimiter = core.getInput("delimiter") || "-"
+
+const pipe = (...fns: Function[]) => (x: any) => fns.reduce((v, f) => f(v), x);
+
+
+const isValidName = (name: string) => {
+	/**
+	 * ASCII letters, numbers and hyphens
+	 */
+	return /^[a-zA-Z0-9\-]+$/g.test(name);
+
+}
+const getBranchName = () => {
+	if (eventName === "delete") {
+		return payload.ref;
+	}
+
+	return ref;
+}
+
+const normalizeName = (ref: string) => {
+	return ref.replace("refs/heads", "");
+}
+
+const transformName = (branchName: string) => {
+	return branchName.replace('/', delimiter);
+}
+
+const featureName = pipe(
+	normalizeName,
+	transformName,
+)(getBranchName());
+
+const valid = isValidName(featureName);
+
+if (!valid) {
+	core.setFailed(`Invalid feature name: ${featureName}`) as never;
+}
+
+
+core.setOutput("feature_name", featureName)
